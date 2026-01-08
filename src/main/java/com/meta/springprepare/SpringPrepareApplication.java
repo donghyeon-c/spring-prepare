421af2e10a1770f0b476cc9cf65088d8a21f20b2
donghyeonShin <dmgbe6437@g.hongik.ac.kr>
2026-01-08 11:18:42 +0900

    refactor: 메인 로직 변경

--- src/main/java/com/meta/springprepare/SpringPrepareApplication.java
+++ src/main/java/com/meta/springprepare/SpringPrepareApplication.java
@@ -8,7 +8,7 @@ public class SpringPrepareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringPrepareApplication.class, args);
-        System.out.println("Hello Spring");
+        System.out.println("게시판 기능 구현 중");
     }
 
 }
